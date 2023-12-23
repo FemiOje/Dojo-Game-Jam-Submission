5340e52b428ba8bdf397496a7761630fbb5008a3
Femi Ojetokun <ojetokunakinwale@gmail.com>
2023-12-23 23:47:48 +0100

    add: project structure

---
+++ Assets/_Scripts/BlockType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType
+{
+    //voxel types
+    Nothing,
+    Air,
+    Grass_Dirt,
+    Dirt,
+    Grass_Stone,
+    Stone,
+    TreeTrunk,
+    TreeLeavesTransparent,
+    TreeLeavesSolid,
+    Water,
+    Sand
+}
